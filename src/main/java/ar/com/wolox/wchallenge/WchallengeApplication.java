015eab99af58b879ea95af4d8527387a67fe076b
johnfreyes <jhonfer96@hotmail.com>
2021-01-06 20:56:07 -0500

    update main class

--- src/main/java/ar/com/wolox/wchallenge/WchallengeApplication.java
+++ src/main/java/ar/com/wolox/wchallenge/WchallengeApplication.java
@@ -2,12 +2,17 @@ package ar.com.wolox.wchallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class })
 public class WchallengeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WchallengeApplication.class, args);
-	}
+	public static void main(String[] args) { SpringApplication.run(WchallengeApplication.class, args); }
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
