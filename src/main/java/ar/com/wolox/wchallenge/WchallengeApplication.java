57f726fa0bb687d808764558a91c31ecae8ed1da
johnfreyes <jhonfer96@hotmail.com>
2021-01-08 21:20:05 -0500

    add h2 bd connection and jpa framework

--- src/main/java/ar/com/wolox/wchallenge/WchallengeApplication.java
+++ src/main/java/ar/com/wolox/wchallenge/WchallengeApplication.java
@@ -2,11 +2,10 @@ package ar.com.wolox.wchallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class WchallengeApplication {
 
 	public static void main(String[] args) { SpringApplication.run(WchallengeApplication.class, args); }
